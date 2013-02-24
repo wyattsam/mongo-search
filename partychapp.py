a220c9bb4c60524581fb6bfb5ece5eba75c7e74e
Jason Rassi <rassi@10gen.com>
2013-02-24 18:51:57 -0500

    add partychapp scraper

---
+++ partychapp.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+import requests
+import json
+import sys
+import pymongo
+
+import settings
+COOKIES = settings.partychapp['cookies']
+
+HANDLER = 'http://chat.10gen.com/logentriesjson/'
+MAX_MESSAGES = 100
+
+MONGO_HOST = 'localhost'
+MONGO_DATABASE = 'xgen'
+MONGO_COLLECTION = 'partychapp'
+
+def fetch(channel):
+  req_data = {
+    'channelName': channel,
+    'limit': MAX_MESSAGES,
+    'offset': 0,
+  }
+  resp = requests.post(url=HANDLER, data=req_data, cookies=COOKIES)
+  if resp.status_code != 200:
+    print "server returned code %d" % resp.status_code
+    return []
+  resp_data = json.loads(resp.text)
+  if 'error' in resp_data:
+    print "server returned error: %s" % resp.text
+    return []
+  return resp_data['entries']
+
+def save(conn, messages):
+  coll = conn[MONGO_DATABASE][MONGO_COLLECTION]
+  for m in messages:
+    coll.insert(m)
+
+if __name__ == '__main__':
+  channel = sys.argv[1]
+  conn = pymongo.MongoClient(MONGO_HOST)
+  messages = fetch(channel)
+  save(conn, messages)
+  print "saved %d messages" % len(messages)
---
+++ settings.default.py
@@ -0,0 +1,5 @@
+partychapp = {
+  'cookies': {
+    'ACSID': '<paste-your-acsid-cookie-here>'
+  }
+}
