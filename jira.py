86ff2b815b6e959ccd4bc0344e55258cf439ca79
Tyler Brock <tyler.brock@gmail.com>
2013-02-24 14:05:21 -0500

    fix jira scraper

--- jira.py
+++ jira.py
@@ -1,39 +1,28 @@
-from base64 import b64encode
-#from flask import Flask, request
-from httplib import HTTPSConnection
-from json import loads
+#!/usr/bin/env python
+import sys
+import requests
 from pymongo import Connection
 from time import sleep
 
-#app = Flask( __name__ )
+MONGO = Connection( 'mongodb://xgen:xgen@localhost:27017/xgen' )
+DB = MONGO['xgen']
+ISSUE_BASE = 'https://jira.mongodb.org/rest/api/latest/issue/'
 
-#@app.route( '/api/jira_hook', methods=[ 'POST' ] )
-def get( num ):
-
-#    issueUrl = request.json[ 'issue' ][ 'self' ]
-    issueUrl = 'https://jira.mongodb.org/rest/api/latest/issue/CS-' + str( num )
-    print issueUrl
-
-    jiraConn = HTTPSConnection( 'jira.mongodb.org' )
-    login = b64encode( b'jiraUser:jiraPass' ).decode( 'ascii' )
-    headers = { 'Authorization' : 'Basic %s' % login }
-    jiraConn.request( 'GET', issueUrl, headers=headers )
-    jiraResponse = jiraConn.getresponse().read()
-    jiraTicketData = loads( jiraResponse )
-    print jiraTicketData
-    jiraTicketData[ '_id' ] = jiraTicketData[ 'key' ]
-
-    mongo = Connection( 'mongoUrl' )
-    db = mongo[ 'search' ]
-    db.authenticate( 'mongoUser', 'mongoPass' )
-    db.jira.save( jiraTicketData )
-
-    return ''
+def get( project, num, username, password ):
+    issue_url = ISSUE_BASE + project + '-' + str( num )
+    print issue_url
+    response = requests.get(issue_url, auth=(username, password), verify=False)
+    jira_ticket_data = response.json()
+    jira_ticket_data[ '_id' ] = jira_ticket_data[ 'key' ]
+    DB.jira.save( jira_ticket_data )
 
 if __name__ == '__main__':
+    if len(sys.argv) < 3:
+        print "usage: jira.py jira_username jira_password"
+        sys.exit(0)
+
+    jira_user, jira_pass = sys.argv[1:3]
+
     for i in range( 1, 5000 ):
-        try:
-            get( i )
-        except:
-            pass
+        get( 'CS', i, jira_user, jira_pass )
         sleep( 2 )
