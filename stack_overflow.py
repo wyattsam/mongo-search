76ad4b28f2d6172141a93e901d76e9b07ed07338
Tyler Brock <tyler.brock@gmail.com>
2013-03-03 15:05:33 -0500

    stack overflow scraper working

--- stack_overflow.py
+++ stack_overflow.py
@@ -1,31 +1,39 @@
-from StringIO import StringIO
-import time
-import urllib
-import json
-import gzip
-import pymongo
-from BeautifulSoup import BeautifulSoup
+#!/usr/bin/env python
+import requests
+from pymongo import Connection
+from time import sleep
 
-username = 'search'
-password = '10gen'
+MONGO = Connection('localhost:27017')
+DB = MONGO['xgen']
+STACK_OVERFLOW = DB['stack_overflow']
+API_BASE = 'https://api.stackexchange.com/2.1/'
+SEARCH_URL = API_BASE + 'search'
 
-#conn = pymongo.Connection('mongodb://50.19.0.111:8000')
-conn = pymongo.Connection()
-db = conn['search']
-#db.authenticate(username, password)
+def save_questions(tag):
+    params = {
+        'site': 'stackoverflow',
+        'tagged': tag,
+        'filter': '!5-2mK)UuZ8I8445X3g0zP75EnxSr_MZe.(FaBo',
+        'sort': 'creation',
+        'order': 'asc',
+        'page': 1,
+        'pagesize': 100
+    }
 
-def fetch_and_insert(page_num, coll):
-    handle = urllib.urlopen('https://api.stackexchange.com/2.1/questions?page=%d&pagesize=100&order=desc&sort=activity&tagged=mongodb&site=stackoverflow&filter=!bULULVX1DHh2eg' % (page_num,))
-    unzipped = gzip.GzipFile(fileobj=StringIO(handle.read()))
-    data = json.load(unzipped)
-    print 'Quota remaining: %s' % (data['quota_remaining'],)
+    while True:
+        result = requests.get(SEARCH_URL, params=params).json()
+        items = result['items']
 
-    for item in data['items']:
-        soup = BeautifulSoup(item['body'])
-        item['body'] = soup.text
-        coll.insert(item)
+        for item in items:
+            item['_id'] = 'SO-' + str(item['question_id'])
+            STACK_OVERFLOW.save(item)
+        
+        if not result['has_more']: break
 
-    time.sleep(data.get('backoff', 1.0)) #Hope I have that json field right. 
-    return data
+        if result.has_key('backoff'):
+            sleep(result['backoff'])
 
-#fetch_and_insert(80, db['stackoverflow'])
+        params['page'] += 1
+
+if __name__ == '__main__':
+    save_questions('mongodb')
