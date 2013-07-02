15ccc3e0873daf6bd108c958662e767fd98f5967
Sam Kleinman <samk@10gen.com>
2013-07-02 19:08:44 -0400

    docs-scraper: make function more generic, add support for ecosystem

--- scripts/docs.py
+++ scripts/docs.py
@@ -16,10 +16,7 @@ SCRAPES = DB['scrapes']
 COMBINED = DB['combined']
 DOCS = DB['docs']
 
-
-def save_doc_pages():
-    file_list_url = 'http://docs.mongodb.org/manual/json/.file_list'
-
+def save_doc_pages(source_name, id_tag, file_list_url):
     file_list = requests.get(file_list_url)
     for file_url in file_list.text.split('\n'):
         if not file_url:
@@ -37,11 +34,11 @@ def save_doc_pages():
         else:
             print(file_url)
             doc = {
-                '_id':  'docs-manual-' + file_json['current_page_name'],
+                '_id':  id_tag + file_json['current_page_name'],
                 'title': file_json['title'],
                 'body': file_json['text'],
                 'url':  file_json['url'],
-                'subsource': 'mongodb',
+                'subsource': source_name,
             }
 
         DOCS.save(doc)
@@ -56,10 +53,21 @@ if __name__ == '__main__':
     })
 
     try:
-        save_doc_pages()
+        save_doc_pages(source_name='mongodb',
+                       id_tag='docs-manual-', 
+                       file_list_url='http://docs.mongodb.org/manual/json/.file_list')
+        save_doc_pages(source_name='ecosystem',
+                       id_tag='docs-ecosystem-', 
+                       file_list_url='http://docs.mongodb.org/ecosystem/json/.file_list')
+      # save_doc_pages(source_name='MMS',
+      #                id_tag='mms-saas-', 
+      #                file_list_url='http://mms.10gen.com/help/json/.file_list')
+      # save_doc_pages(source_name='MMS On-Prem',
+      #                id_tag='mms-hosted-', 
+      #                file_list_url='http://mms.10gen.com/help-hosted/current/json/.file_list')
+
         SCRAPES.update({'_id': scrape},
             { '$set': { 'state': 'complete', 'end': datetime.now()} })
-
     except Exception as error:
         SCRAPES.update({'_id': scrape},
             { '$set': { 'state': 'failed', 'error': error} })
