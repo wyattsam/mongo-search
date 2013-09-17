6acb5996d7b86b8550398cdae060884023e6fc3d
Tyler Brock <tyler.brock@gmail.com>
2013-09-17 09:24:25 -0400

    add profiles scraper

---
+++ scripts/profiles.py
@@ -0,0 +1,23 @@
+from scrapers import JSONScraper
+
+
+class ProfilesScraper(JSONScraper):
+    NAME = 'profiles'
+    API_BASE = 'https://corp.10gen.com/api/'
+    EMPLOYEE_URL = API_BASE + 'employee'
+
+    def __init__(self, credentials):
+        self.credentials = credentials
+
+    def scrape_employee(self, employee):
+        employee_id = employee['uri'].strip('/api/employee/')
+        employee['_id'] = 'employee-' + employee_id
+        return employee
+
+    def scrape(self):
+        params = {'expand': 'team'}
+        result = self.get_json(self.EMPLOYEE_URL, params=params,
+                               auth=self.credentials, digest=True)
+
+        for employee in result['employees']:
+            yield self.scrape_employee(employee)
