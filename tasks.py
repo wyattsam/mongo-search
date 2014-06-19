d81c8c982d143fd647f934f88fc8434ff0637565
Jed Estep <austin.estep@10gen.com>
2014-06-19 16:36:19 -0400

    self-repeating tasks, each scrape is independent

--- tasks.py
+++ tasks.py
@@ -3,11 +3,19 @@ import config.celery as celery_settings
 import config.duckduckmongo as search_settings
 from scrape import ScrapeRunner
 
-app = Celery('celery')
+app = Celery('tasks')
 app.config_from_object(celery_settings)
 
-scraper = ScrapeRunner(search_settings.CONFIG)
+cfg = search_settings.CONFIG
+scraper = ScrapeRunner(cfg)
+
+sources = [k for k in cfg.keys() if k[0] != '_']
 
 @app.task
-def scrapeall():
-    scraper.runall()
+def scrape_source(source):
+    scraper.run(cfg[source]['scraper'])
+    scrape_stackoverflow.delay(source)
+
+if __name__ == '__main__':
+    for source in sources:
+        scrape_source.delay(source)
