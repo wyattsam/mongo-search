0ba5cde379a2d593ccbe4747ee775e978f6be072
Jed Estep <austin.estep@10gen.com>
2014-06-26 10:29:33 -0400

    celery now auto-restarts tasks; previous bug was a logspam issue

--- config/celery.py
+++ config/celery.py
@@ -1,5 +1,5 @@
 BROKER_URL = 'mongodb://localhost:27017/celery_tasks'
 
-CELERY_ACCEPT_CONTENT = ['json', 'msgpack']
-CELERY_TASK_SERIALIZER = 'msgpack'
-CELERY_RESULT_SERIALIZER = 'msgpack'
+CELERY_ACCEPT_CONTENT = ['json', 'pickle']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
--- tasks.py
+++ tasks.py
@@ -8,13 +8,15 @@ app.config_from_object(celery_settings)
 
 cfg = search_settings.CONFIG
 
-sources = [k for k in cfg.keys() if k[0] != '_']
+#sources = [k for k in cfg.keys() if k[0] != '_']
+sources = ['docs']
 
 @app.task
 def scrape_source(source):
     scraper = ScrapeRunner(cfg, snames=[source])
     scraper.do_setup()
     scraper.runall()
+    scrape_source.delay(source)
 
 if __name__ == '__main__':
     for source in sources:
