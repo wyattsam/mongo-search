2ba0b6022bf0ad4ef46319e15bfba225503b2f4c
Jed Estep <austin.estep@10gen.com>
2014-06-27 11:42:42 -0400

    more automation plumbing

--- fabfile.py
+++ fabfile.py
@@ -1,17 +1,17 @@
 from fabric.api import local, cd, run, env, settings
 
-env.user = 'ubuntu'
-env.hosts = ['ec2-54-85-5-163.compute-1.amazonaws.com']
-env.key_filename = '~/.ssh/mongodb-search.pem'
-
 requirements = [
     'git',
     'python-dev',
     'python-pip',
     'mongodb'
     ]
-
 code_dir = '/home/ubuntu/search'
+hostname = 'ec2-54-85-5-163.compute-1.amazonaws.com'
+
+env.user = 'ubuntu'
+env.hosts = [hostname]
+env.key_filename = '~/.ssh/mongodb-search.pem'
 
 def commit():
     local("git add -p && git commit")
@@ -25,9 +25,12 @@ def prepare_deploy():
 
 def install_reqs():
     run('sudo apt-get install ' + ' '.join(requirements).strip()) 
+
+def install_libs():
     with settings(warn_only=True):
         if not run('test -d %s' % code_dir).failed:
             with cd(code_dir):
+                run('git pull')
                 run('sudo pip install -r requirements.txt')
 
 def deploy():
@@ -36,7 +39,8 @@ def deploy():
             run('mkdir %s' % code_dir)
             install_reqs()
             run('git clone https://github.com/10gen/search %s' % code_dir)
+            install_libs()
     with cd(code_dir):
         run("git pull")
-        #run("sudo restart search")
+        local('scp -i %s ~/dev/search/config/duckduckmongo.py ubuntu@%s:%s/config/' % (env.key_filename, hostname, code_dir))
         run("gunicorn app:app")
